45e5ae0b9e7f6eb3ade0871ffa4026c2f3a5c5f1
Rendy <rendy.de.p@gmail.com>
2020-08-22 18:57:59 +0700

    Add experience post type

---
+++ wp-content/themes/muslimarkt/includes/abstracts/class-type.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Post type abstract class.
+ *
+ * @author Rendy
+ * @package Muslimarkt
+ * @version 0.0.1
+ */
+
+namespace Muslimarkt\Type;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Muslimarkt\Type\Type' ) ) {
+
+	/**
+	 * Class Type
+	 *
+	 * @package Muslimarkt\Type
+	 */
+	abstract class Type {
+
+		/**
+		 * Slug variable.
+		 *
+		 * @var string
+		 */
+		protected $slug = 'custom';
+
+		/**
+		 * Args variable.
+		 *
+		 * @var array
+		 */
+		protected $args = array();
+
+		/**
+		 * Type constructor.
+		 */
+		protected function __construct() {
+
+			// Add action to register custom post type.
+			add_action( 'init', array( $this, 'register_post_type' ) );
+
+			// Prepare default args.
+			$default_args = array(
+				'label'  => ucfirst( $this->slug . 's' ),
+				'public' => true,
+			);
+
+			// Merge args.
+			$this->args = wp_parse_args( $this->args, $default_args );
+		}
+
+		/**
+		 * Register post type.
+		 */
+		public function register_post_type() {
+			register_post_type( $this->slug, $this->args );
+		}
+	}
+}
---
+++ wp-content/themes/muslimarkt/includes/types/class-experience.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Experience post type class.
+ *
+ * @author Rendy
+ * @package Muslimarkt
+ */
+
+namespace Muslimarkt\Type;
+
+use Muslimarkt\Singleton;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Muslimarkt\Type\Experience' ) ) {
+
+	/**
+	 * Class Experience
+	 *
+	 * @package Muslimarkt\Type
+	 */
+	final class Experience extends Type {
+		use Singleton;
+
+		/**
+		 * Override slug variable.
+		 *
+		 * @var string
+		 */
+		protected $slug = 'experience';
+
+		/**
+		 * Override post type args.
+		 *
+		 * @var array
+		 */
+		protected $args = array(
+			'label' => 'Experiences',
+		);
+	}
+
+	Experience::init();
+}
