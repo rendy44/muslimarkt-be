e65a8eee582ad67d2e5283324eda9e453a462d0f
Rendy <rendy.de.p@gmail.com>
2020-08-27 19:33:23 +0700

    Add register api class

---
+++ wp-content/themes/muslimarkt/includes/api/class-register.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Register api class.
+ *
+ * @author Rendy
+ * @package Muslimarkt
+ * @version 0.0.1
+ */
+
+namespace Muslimarkt\Rest;
+
+use Muslimarkt\Auth;
+use Muslimarkt\Singleton;
+use Muslimarkt\Model\User;
+use WP_REST_Request;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Muslimarkt\Rest\Register' ) ) {
+
+	/**
+	 * Class Register
+	 *
+	 * @package Muslimarkt
+	 */
+	final class Register extends Rest {
+		use Singleton;
+
+		/**
+		 * Override privilege variable.
+		 *
+		 * @var bool
+		 */
+		protected $is_require_key = false;
+
+		/**
+		 * Override endpoint url variable.
+		 *
+		 * @var string
+		 */
+		protected $endpoint = 'register';
+
+		/**
+		 * Override use post variable.
+		 *
+		 * @var bool
+		 */
+		protected $use_post = true;
+
+		/**
+		 * Callback for get method.
+		 *
+		 * @param WP_REST_Request $request request object.
+		 */
+		function get_callback( $request ) {
+			// TODO: Implement get_callback() method.
+		}
+
+		/**
+		 * Callback for post method.
+		 *
+		 * @param WP_REST_Request $request request object.
+		 */
+		function post_callback( $request ) {
+
+			// Instance new auth.
+			$auth = new Auth( $request );
+
+			// Process create a user.
+			$user = new User( false, $auth->get_args() );
+
+			// Validate content.
+			$auth->content_on_error( $user->message );
+			$auth->content_on_success( __( 'Pendaftaran sukses, silahkan masuk dengan email dan kata sandi', 'muslimarkt' ) );
+			$auth->parse_api( $user );
+		}
+
+		/**
+		 * Callback for delete method.
+		 *
+		 * @param WP_REST_Request $request request object.
+		 */
+		function delete_callback( $request ) {
+			// TODO: Implement delete_callback() method.
+		}
+
+		/**
+		 * Callback for put method.
+		 *
+		 * @param WP_REST_Request $request request object.
+		 */
+		function put_callback( $request ) {
+			// TODO: Implement put_callback() method.
+		}
+	}
+
+	Register::init();
+}
