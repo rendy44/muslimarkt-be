5fe6dbbdcd15ea93532f9d039f4a32acb56feca8
Rendy <rendy.de.p@gmail.com>
2020-09-23 20:20:11 +0700

    Add dummy media uploader

---
+++ wp-content/themes/muslimarkt/includes/api/class-media.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Media api class.
+ *
+ * @author Rendy
+ * @package Muslimarkt
+ * @version 0.0.1
+ */
+
+namespace Muslimarkt\Rest;
+
+use Muslimarkt\Abstracts\Rest;
+use Muslimarkt\Auth;
+use Muslimarkt\Singleton;
+use WP_REST_Request;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Muslimarkt\Rest\Media' ) ) {
+
+	/**
+	 * Class Media
+	 *
+	 * @package Muslimarkt\Rest
+	 */
+	final class Media extends Rest {
+		use Singleton;
+
+		/**
+		 * Override endpoint url variable.
+		 *
+		 * @var string
+		 */
+		protected $endpoint = 'media';
+
+		/**
+		 * Override use get variable.
+		 *
+		 * @var bool
+		 */
+		protected $use_post = true;
+
+		/**
+		 * Callback for post method.
+		 *
+		 * @param WP_REST_Request $request request object.
+		 */
+		function post_callback( $request ) {
+
+			// Instance a new auth.
+			$auth = new Auth( $request );
+
+			// Create a callback.
+			$auth->success_callback(
+				function () use ( $auth ) {
+
+					// Instance of the uploader
+					// Instance a new user.
+					// $user = new \Muslimarkt\Model\User( $auth->user_id );
+					//
+					// Activate user account.
+					// $user->activate_account();
+				},
+				false
+			);
+
+			// Media the request.
+			$auth->validate();
+		}
+
+		/**
+		 * Callback for delete method.
+		 *
+		 * @param WP_REST_Request $request request object.
+		 */
+		function delete_callback( $request ) {
+			// TODO: Implement delete_callback() method.
+		}
+
+		/**
+		 * Callback for put method.
+		 *
+		 * @param WP_REST_Request $request request object.
+		 */
+		function put_callback( $request ) {
+			// TODO: Implement put_callback() method.
+		}
+
+		/**
+		 * Callback for getting detail method.
+		 *
+		 * @param WP_REST_Request $request request object.
+		 */
+		function get_detail_callback( $request ) {
+			// TODO: Implement get_detail_callback() method.
+		}
+
+		/**
+		 * Callback for get method.
+		 *
+		 * @param WP_REST_Request $request request object.
+		 */
+		function get_callback( $request ) {
+			// TODO: Implement get_callback() method.
+		}
+	}
+
+	Media::init();
+}
---
+++ wp-content/themes/muslimarkt/includes/models/class-media.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Class media.
+ *
+ * @author Rendy
+ * @package Muslimarkt
+ * @version 0.0.1
+ */
+
+namespace Muslimarkt\Model;
+
+use Muslimarkt\Result;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Muslimarkt\Model\Media' ) ) {
+
+	/**
+	 * Class Media
+	 *
+	 * @package Muslimarkt
+	 */
+	final class Media {
+		use Result;
+
+		private $uploaded_file;
+
+		public function __construct( $file ) {
+
+			// Require core file.
+			require_once ABSPATH . 'wp-admin/includes/admin.php';
+
+			// Process upload.
+			$upload = wp_handle_upload( $file );
+
+			// Validate upload.
+			if ( empty( $upload['error'] ) && empty( $upload['upload_error_handler'] ) ) {
+
+			} else {
+
+				// Save error message.
+				$this->message[] = $upload['error'] ? $upload['error'] : '';
+				$this->message[] = $upload['upload_error_handler'] ? $upload['upload_error_handler'] : '';
+			}
+
+		}
+
+	}
+}
